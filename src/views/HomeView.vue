2e161caa50d199c947020dbb3bb92cc2f904dcdc
AyrtonlinOn <ayrtonlimaol22@gmail.com>
2024-04-16 20:49:22 -0300

    fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,17 +20,23 @@ onMounted(() => {
   <main>
     <div class="container">
       <div class="row mt-4">
-        <div class="col-sm-12 col-md-06">
+        <div class="col-sm-12 col-md-6">
           <div class="card" style="width: 18rem;">
             <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" class="card-img-top" alt="...">
-          </div>
           <div class="card-body">
             <h5 class="card-title">Card Title</h5>
             <p class="card-text">Um texto resumido e pequeno aqui</p>
           </div>
         </div>
-        <div class="col-sm-12 col-md-06">
-          OLÁ
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem">
+            <ul class="list-group">
+              <li class="list-group-item" v-for="pokemon in pokemons" :key="pokemon.name">
+                {{ pokemon.name }}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
